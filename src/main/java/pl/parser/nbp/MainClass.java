e39261d9c8270fbe2da35059efa73342af2f0735
marlop92 <mlopatka.krk@gmail.com>
2019-03-29 14:11:54 +0100

    Initial commit

---
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -0,0 +1,13 @@
+package pl.parser.nbp;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        //get and displays current NBP currencies for selling and buying
+        //input: USD, EUR, CHF, GBP, Date from , Date to - (publication date)
+        //output: average buy rate, sales standard deviation
+        //input data passed via args
+        //displayed on standard output
+    }
+}
---
+++ src/test/java/pl/parser/nbp/services/CurrencyServiceTest.java
@@ -0,0 +1,16 @@
+package pl.parser.nbp.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyServiceTest {
+
+    CurrencyService service;
+
+    @Test
+    public void shouldReturnValidCurrencyInfo() {
+
+    }
+
+}
