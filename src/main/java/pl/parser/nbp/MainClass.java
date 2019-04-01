16468b069f746b7922d032df7b2415170cfec159
marlop92 <mlopatka.krk@gmail.com>
2019-04-02 00:06:09 +0200

    Input params parser

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -2,16 +2,16 @@ package pl.parser.nbp;
 
 import pl.parser.nbp.model.CurrencyStats;
 import pl.parser.nbp.model.CurrencyStatsRequest;
+import pl.parser.nbp.services.currency.CurrencyService;
 import pl.parser.nbp.services.currency.SimpleConcurrentCurrencyService;
-
-import java.time.LocalDate;
+import pl.parser.nbp.util.InputParser;
 
 public class MainClass {
 
     public static void main(String[] args) {
-        SimpleConcurrentCurrencyService simpleConcurrentCurrencyService = new SimpleConcurrentCurrencyService();
-        CurrencyStats stats = simpleConcurrentCurrencyService.getCurrencyStats(new CurrencyStatsRequest("USD",
-                LocalDate.parse("2015-04-13"), LocalDate.now()));
+        CurrencyStatsRequest request = InputParser.parseInput(args);
+        CurrencyService currencyService = new SimpleConcurrentCurrencyService();
+        CurrencyStats stats = currencyService.getCurrencyStats(request);
         System.out.println(stats);
     }
 }
---
+++ src/main/java/pl/parser/nbp/exceptions/InvalidInputException.java
@@ -0,0 +1,8 @@
+package pl.parser.nbp.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
